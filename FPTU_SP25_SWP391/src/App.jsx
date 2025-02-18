dc12ae59369621e7b64ad6d6bf7e1d2f81af5dde
Dawn-nguyn1 <mnmilanstc4@gmail.com>
2025-02-18 10:29:02 +0700

    updated footer 1

--- FPTU_SP25_SWP391/src/App.jsx
+++ FPTU_SP25_SWP391/src/App.jsx
@@ -11,24 +11,29 @@ import SignIn from './page/SignIn';
 import ForgotPassword from './page/ForgotPassword';
 import BlogDetail from './page/BlogDetails';
 import './App.css'
+import Footer from './component/Footer';
 
 function App() {
 
 
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/booking_page" element={<BookingPage />} />
-        <Route path='/blog/:id' element={<BlogDetail />} />
-        <Route path="/cart_page" element={<CartPage />} />
-        <Route path="/products" element={<ProductListPage />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/sign_in" element={<SignIn />} />
-        <Route path="/forgot_password" element={<ForgotPassword />} />
-      </Routes>
+      <div className="App">
+        <Header />
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/booking_page" element={<BookingPage />} />
+            <Route path="/cart_page" element={<CartPage />} />
+            <Route path="/products" element={<ProductListPage />} />
+            <Route path="/about" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/sign_in" element={<SignIn />} />
+            <Route path="/forgot_password" element={<ForgotPassword />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   )
 }
