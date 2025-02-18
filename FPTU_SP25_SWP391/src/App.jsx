213e85c160c12649233183755fecb1d1a8db6611
VuHo <vuho3244@gmail.com>
2025-02-18 13:10:55 +0700

    Revert "updated footer 1"
    This reverts commit dc12ae59369621e7b64ad6d6bf7e1d2f81af5dde.

--- FPTU_SP25_SWP391/src/App.jsx
+++ FPTU_SP25_SWP391/src/App.jsx
@@ -10,34 +10,26 @@ import Header from './component/Header';
 import SignIn from './page/SignIn';
 import ForgotPassword from './page/ForgotPassword';
 import './App.css'
-import Footer from './component/Footer';
-// import Content from './component/Content';
+
 function App() {
 
 
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <main className="content">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/booking_page" element={<BookingPage />} />
-            <Route path="/cart_page" element={<CartPage />} />
-            <Route path="/products" element={<ProductListPage />} />
-            <Route path="/about" element={<Blog />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/sign_in" element={<SignIn />} />
-            <Route path="/forgot_password" element={<ForgotPassword />} />
-          </Routes>
-        </main>
-        {/* <Content />
-        <Content />
-        <Content />
-        <Content />
-        <Content /> */}
-        <Footer />
-      </div>
+
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/booking_page" element={<BookingPage />} />
+        <Route path='/blog/:id' element={<BlogDetail />} />
+        <Route path="/cart_page" element={<CartPage />} />
+        <Route path="/products" element={<ProductListPage />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/sign_in" element={<SignIn />} />
+        <Route path="/forgot_password" element={<ForgotPassword />} />
+      </Routes>
+
     </Router>
   );
 }
