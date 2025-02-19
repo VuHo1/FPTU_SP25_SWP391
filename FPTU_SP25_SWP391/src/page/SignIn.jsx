f6ef25ce93cd478cfd2601afd3d2cf618f461bbb
loi2003 <loitntse171276@fpt.edu.vn>
2025-02-19 10:43:05 +0700

    update header homepage and signin darkmode

--- FPTU_SP25_SWP391/src/page/SignIn.jsx
+++ FPTU_SP25_SWP391/src/page/SignIn.jsx
@@ -1,113 +1,112 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import Swal from "sweetalert2";
-// import { jwtDecode } from "jwt-decode";
-// import { login } from "../../../api/testApi";
+import Swal from "sweetalert2";
+import { jwtDecode } from "jwt-decode";
+import { login } from "../api/testApi";
 
 export default function SignIn({darkMode}) {
-  // const [emali, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [errorMessage, setErrorMessage] = useState("");
-  // const [inputType, setInputType] = useState("password");
-  // const navigate = useNavigate();
+  const [emali, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [inputType, setInputType] = useState("password");
+  const navigate = useNavigate();
 
-  // const handleEmailChange = (event) => {
-  //   setEmail(event);
-  // };
+  const handleEmailChange = (event) => {
+    setEmail(event);
+  };
 
-  // const handlePasswordChange = (event) => {
-  //   setPassword(event);
-  // };
+  const handlePasswordChange = (event) => {
+    setPassword(event);
+  };
 
-  // // Nếu muốn thêm chức năng hiển thị mật khẩu khi nhấn nút
-  // const togglePasswordVisibility = () => {
-  //   setInputType(inputType === "password" ? "text" : "password");
-  // };
+  // Nếu muốn thêm chức năng hiển thị mật khẩu khi nhấn nút
+  const togglePasswordVisibility = () => {
+    setInputType(inputType === "password" ? "text" : "password");
+  };
 
-  // const handleSave = async () => {
-  //   if (!emali || !password) {
-  //     Swal.fire({
-  //       icon: "error",
-  //       title: "Oops...",
-  //       text: "Please fill in all required fields!",
-  //     });
-  //     setIsLoading(false);
-  //     return;
-  //   }
-  //   const data = {
-  //     email: emali,
-  //     password: password,
-  //   };
+  const handleSave = async () => {
+    if (!emali || !password) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please fill in all required fields!",
+      });
+      setIsLoading(false);
+      return;
+    }
+    const data = {
+      email: emali,
+      password: password,
+    };
 
-  //   setIsLoading(true);
+    setIsLoading(true);
 
-  //   try {
-  //     const response = await login(data);
+    try {
+      const response = await login(data);
 
-  //     Swal.fire({
-  //       icon: "success",
-  //       title: "Login success!",
-  //       text: response?.data?.message || "Successfully",
-  //     });
+      Swal.fire({
+        icon: "success",
+        title: "Login success!",
+        text: response?.data?.message || "Successfully",
+      });
 
-  //     console.log("Data: ", response);
+      console.log("Data: ", response);
 
-  //     const token = response.data.data.accessToken;
-  //     localStorage.setItem("token", token);
-  //     console.log("Token: ", token);
+      const token = response.data.data.accessToken;
+      localStorage.setItem("token", token);
+      console.log("Token: ", token);
 
-  //     // Giải mã token để lấy thông tin role
-  //     const decodedToken = jwtDecode(token);
-  //     const role =
-  //       decodedToken[
-  //         "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
-  //       ]; // Giả sử 'role' là key chứa role trong token
-  //     localStorage.setItem("role", role);
-  //     console.log("decode: ", decodedToken);
+      // Giải mã token để lấy thông tin role
+      const decodedToken = jwtDecode(token);
+      const role =
+        decodedToken[
+          "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
+        ]; // Giả sử 'role' là key chứa role trong token
+      localStorage.setItem("role", role);
+      console.log("decode: ", decodedToken);
 
-  //     const full_name =
-  //       decodedToken[
-  //         "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"
-  //       ];
-  //     localStorage.setItem("full_name", full_name);
+      const full_name =
+        decodedToken[
+          "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"
+        ];
+      localStorage.setItem("full_name", full_name);
 
-  //     localStorage.setItem("userId", decodedToken.id);
+      localStorage.setItem("userId", decodedToken.id);
 
-  //     // Chuyển hướng dựa trên role
-  //     if (role === "User") {
-  //       navigate("/");
-  //     } else if (role === "Staff") {
-  //       navigate("/shopProfile/shop");
-  //     }
-  //     console.log("Data: ", response);
-  //   } catch (error) {
-  //     let errorMessage = "An unknown error occurred.";
+      // Chuyển hướng dựa trên role
+      if (role === "User") {
+        navigate("/");
+      } else if (role === "Staff") {
+        navigate("/shopProfile/shop");
+      }
+      console.log("Data: ", response);
+    } catch (error) {
+      let errorMessage = "An unknown error occurred.";
 
-  //     try {
-  //       const parsedResponse = JSON.parse(error.request.response);
-  //       errorMessage = parsedResponse.message || errorMessage;
-  //     } catch (parseError) {
-  //       console.error("Error parsing JSON response:", parseError);
-  //     }
+      try {
+        const parsedResponse = JSON.parse(error.request.response);
+        errorMessage = parsedResponse.message || errorMessage;
+      } catch (parseError) {
+        console.error("Error parsing JSON response:", parseError);
+      }
 
-  //     Swal.fire({
-  //       icon: "error",
-  //       title: "Please check your input!!!",
-  //       text: errorMessage,
-  //     });
-  //     setIsLoading(false);
-  //     console.error("An error occurred while sending the API request:", error);
-  //   }
-  // };
+      Swal.fire({
+        icon: "error",
+        title: "Please check your input!!!",
+        text: errorMessage,
+      });
+      setIsLoading(false);
+      console.error("An error occurred while sending the API request:", error);
+    }
+  };
 
-  // // Hàm xử lý khi nhấn phím Enter
-  // const handleKeyDown = (event) => {
-  //   if (event.key === "Enter") {
-  //     handleSave();
-  //   }
-  // };
- 
+  // Hàm xử lý khi nhấn phím Enter
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSave();
+    }
+  };
  
     return (
       <div
